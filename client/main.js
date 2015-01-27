22db37e889b22573004bf5247c238b9d17a394bd
Benjamin Dirgo <benjamindirgo@Benjamins-MacBook-Air.local>
2015-01-26 21:24:15 -0700

    reorginized it all

--- client/main.js
+++ client/main.js
@@ -1,11 +1,4 @@
-
-// Create a MongoDB Collection
-PlayersList = new Mongo.Collection('players');
-
-// Code that only runs on the client (within the web browser)
-if(Meteor.isClient){
-
-  Meteor.subscribe('thePlayers');
+Meteor.subscribe('thePlayers');
 
   // Helper functions execute code within templates
   Template.leaderboard.helpers({
@@ -59,15 +52,15 @@ if(Meteor.isClient){
           var selectedPlayer = Session.get('selectedPlayer');
 
           //increment just the score value of the player selected
-          PlayersList.update(selectedPlayer, {$inc:{score:5}});
+          Meteor.call('modifyPlayerScore',selectedPlayer,5);
       },
       'click .decrement': function(){
 
           //find the current player selected
           var selectedPlayer = Session.get('selectedPlayer');
 
-          //increment just the score value of the player selected
-          PlayersList.update(selectedPlayer, {$inc:{score:-5}});
+          //decrement just the score value of the player selected
+          Meteor.call('modifyPlayerScore', selectedPlayer,-5);
       },
       'click .remove': function(){
         //Confirm they wnat to remove the player
@@ -78,7 +71,7 @@ if(Meteor.isClient){
           var selectedPlayer = Session.get('selectedPlayer');
 
           // Remove a document from the collection
-          PlayersList.remove(selectedPlayer);
+          Meteor.call('removePlayerData', selectedPlayer);
         };
       }
   });
@@ -92,30 +85,8 @@ if(Meteor.isClient){
         //get the value of the players name in the form 
         var playerNameVar = event.target.playerName.value;
 
-        //store the Id of the currently logged in user
-        var currentUserId = Meteor.userId();
-
-        //add the name passed in and assign it with a default value of 0
-        PlayersList.insert({
-          name: playerNameVar,
-          score: 0,
-          createdBy: currentUserId
-        });
-
         //remove the text from the screen
         event.target.playerName.value = "";
+        Meteor.call('insertPlayerData', playerNameVar);
       }
   });
-
-}
-
-// Code that only runs on the server (where the application is hosted)
-if(Meteor.isServer){
-  Meteor.publish('thePlayers', function(){
-    var currentUserId = this.userId;
-    return PlayersList.find({createdBy:currentUserId});
-  });
-}
-
-
-
---
+++ collections.js
@@ -0,0 +1,4 @@
+
+// Create a MongoDB Collection
+PlayersList = new Mongo.Collection('players');
+
